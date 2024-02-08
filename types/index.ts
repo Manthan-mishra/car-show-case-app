3e5dcc441f1095bfa540ac9c99f44b9e36d260c7
Manthan-mishra <manthan9481147615@gmail.com>
2024-02-08 22:05:44 +0530

    navbar and hero design complete

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,8 @@
 import { MouseEventHandler } from "react";
 
-export interface ICustomButton{
-    title:string;
-    containerStyles?:string;
-    handleClick?:MouseEventHandler<HTMLButtonElement>;
+export interface ICustomButton {
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
 }
